e222f98be3be781f101ee5b07b332175d8307a2b
Yi Hu <yhu28@ncsu.edu>
2021-09-13 08:55:58 -0400

    edit test file

--- test.py
+++ test.py
@@ -3,10 +3,32 @@ import pickle
 import os
 from os import walk
 
-file_path = 'C:\\Users\\yhu28\\Downloads\\outputPecanData\\complete\\'
+file_path = 'C:\\Users\\yhu28\\Downloads\\outputPecanData\\PecanStreet_Reconstructed\\'
 for (dirpath, dirnames, filenames) in walk(file_path):
     for file in filenames:
+        if file != '1042':
+            continue
         fullname = os.path.join(dirpath, file)
         with open(fullname, 'rb') as f:
             data = pickle.load(f)
-            print(data)
+            # print(data)
+
+file_name = 'C:\\Users\\yhu28\\Downloads\\outputPecanData\\PecanStreet_Separately\\1min\\1042'
+with open(file_name, 'rb') as f:
+    data = pickle.load(f)
+    print(data)
+
+file_name = 'C:\\Users\\yhu28\\Downloads\\outputPecanData\\PecanStreet_Separately\\15min\\1042'
+with open(file_name, 'rb') as f:
+    data = pickle.load(f)
+    print(data)
+
+file_name = 'C:\\Users\\yhu28\\Downloads\\outputPecanData\\PecanStreet_Separately\\water\\1042'
+with open(file_name, 'rb') as f:
+    data = pickle.load(f)
+    print(data)
+
+file_name = 'C:\\Users\\yhu28\\Downloads\\outputPecanData\\PecanStreet_Separately\\gas\\1042'
+with open(file_name, 'rb') as f:
+    data = pickle.load(f)
+    print(data)
